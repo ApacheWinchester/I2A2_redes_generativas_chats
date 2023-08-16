f2f561fdd25f901a55c162b5c6c9d0a2e5a08bbc
Henrique Santos Zangerolame de Oliveira <henriquesatoszan@hotmail.com>
2023-08-16 16:38:45 -0300

    geral

--- public/js/home.js
+++ public/js/home.js
@@ -1,67 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const backgroundVideo = document.getElementById('backgroundVideo');
-	backgroundVideo.pause();
-    
-});
+  const drFreudButton = document.getElementById('drFreudButton');
+  const drElizaButton = document.getElementById('drElizaButton');
+  const drBrizaaButton = document.getElementById('drBrizaaButton');
 
 // animaçao Dr.Freud
-function openDoor() {
-    const bgVideo = document.getElementById('backgroundVideo');
-    bgVideo.playbackRate = 2.0;
-    bgVideo.play();
-	setTimeout(() => {
-		window.location.href = '/html/freud.html';
-	}, 3750);
-    var buttonElement = document.querySelector('.button');
-    var h1Element = document.querySelector('h1');
-    var pElement = document.querySelector('p');
-    var dreliza = document.querySelector('#drElizaButton');
-    var drbriza = document.querySelector('#drBrizaaButton');
-    buttonElement.style.display = 'none';
-    h1Element.style.display = 'none';
-    pElement.style.display = 'none';
-    dreliza.style.display = 'none';
-    drbriza.style.display = 'none';
-}
+drFreudButton.addEventListener('click', () => {
+  // Redireciona para a página desejada
+  window.location.href = '/html/freud.html';
+});
 
 
-function openDoor1() {
-  const bgVideo = document.getElementById('backgroundVideo');
-  bgVideo.playbackRate = 2.0;
-  bgVideo.play();
-setTimeout(() => {
+drElizaButton.addEventListener('click', () => {
+  // Redireciona para a página desejada
   window.location.href = '/html/eliza.html';
-}, 3750);
-  var buttonElement = document.querySelector('.button');
-  var h1Element = document.querySelector('h1');
-  var pElement = document.querySelector('p');
-  var dreliza = document.querySelector('#drElizaButton');
-  var drbriza = document.querySelector('#drBrizaaButton');
-  buttonElement.style.display = 'none';
-  h1Element.style.display = 'none';
-  pElement.style.display = 'none';
-  dreliza.style.display = 'none';
-  drbriza.style.display = 'none';
-}
+});
 
-function openDoor2() {
-  const bgVideo = document.getElementById('backgroundVideo');
-  bgVideo.playbackRate = 2.0;
-  bgVideo.play();
-setTimeout(() => {
+drBrizaaButton.addEventListener('click', () => {
+  // Redireciona para a página desejada
   window.location.href = '/html/briza.html';
-}, 3750);
-  var buttonElement = document.querySelector('.button');
-  var h1Element = document.querySelector('h1');
-  var pElement = document.querySelector('p');
-  var dreliza = document.querySelector('#drElizaButton');
-  var drbriza = document.querySelector('#drBrizaaButton');
-  buttonElement.style.display = 'none';
-  h1Element.style.display = 'none';
-  pElement.style.display = 'none';
-  dreliza.style.display = 'none';
-  drbriza.style.display = 'none';
-}
+});
 
 
 
+});
--- public/js/key.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = "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";
-
-module.exports = API_KEY;
